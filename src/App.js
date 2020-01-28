e2ad510a03f1bad077d2e69355ed2faa0f88e8f8
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-01-28 17:08:31 -0300

    updated example to use React SDK, and fixed vulnerabilities

--- src/App.js
+++ src/App.js
@@ -1,89 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { SplitFactory } from '@splitsoftware/splitio';
+import React from 'react';
+import { Route, Link, BrowserRouter } from 'react-router-dom'
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import PageUsingComponents from './pages/PageUsingComponents';
+import PageUsingHOCs from './pages/PageUsingHOCs';
+import PageUsingHooks from './pages/PageUsingHooks';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <nav className="crumbs">
+            <ul>
+              <li className="crumb">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="crumb">
+                <Link to="/components">Page using components</Link>
+              </li>
+              <li className="crumb">
+                <Link to="/hocs">Page using HOCs</Link>
+              </li>
+              <li className="crumb">
+                <Link to="/hooks">Page using hooks</Link>
+              </li>
+            </ul>
+          </nav>
 
-    this.state = {
-      isSdkReady: false,
-      treatmentTestSplit: 'control',
-      treatmentTestAnotherSplit: 'control',
-      treatmentTestSomethingElse: 'control',
-    };
-  }
+          <Route path="/" exact component={HomePage} />
+          <Route path="/components" component={PageUsingComponents} />
+          <Route path="/hocs" component={PageUsingHOCs} />
+          <Route path="/hooks" component={PageUsingHooks} />
 
-  componentDidMount() {
-    // note that we are using for this hello world app the off-the-grid mode
-    // on authorizationKey replace with your api key
-    // features object is used only for off-the-grid mode
-    const config = {
-      core: {
-        authorizationKey: 'localhost',
-        key: 'customer-key'
-      },
-      features: {
-        'Test_Split': 'on',
-        'Test_Another_Split': 'dark',
-        'Test_Something_Else': 'off'
-      }
-    };
-
-    const sdkFactory = SplitFactory(config)
-    const sdkClient = sdkFactory.client();
-
-    // we need to wait the sdk to finish loading and then we can retrieve the
-    // treatments for each split
-    // http://docs.split.io/docs/javascript-sdk-overview#section-how-to-reflect-sdk-updates-in-your-app
-    sdkClient.on(sdkClient.SDK_READY, () => {
-      this.setState({
-        // we set the is ready flag to true
-        isSdkReady: true,
-        // retr
-        treatmentTestSplit: sdkClient.getTreatment('Test_Split'),
-        treatmentTestAnotherSplit: sdkClient.getTreatment('Test_Another_Split'),
-        treatmentTestSomethingElse: sdkClient.getTreatment('Test_Something_Else'),
-      });
-    });
-
-    sdkClient.on(sdkClient.SDK_UPDATE, () => {
-      // we could evaluate again the sdk if there was any change
-    });
-  }
-
-  render() {
-    const {
-      treatmentTestSplit,
-      treatmentTestAnotherSplit,
-      treatmentTestSomethingElse,
-      isSdkReady
-    } = this.state;
-
-    // we only want to show something when the sdk is ready to use
-    if (!isSdkReady) {
-      return null;
-    }
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Split JS SDK Hello World!</h2>
         </div>
-        <div className="App-section">
-          Split <strong>Test_Split</strong> return treatment <strong>{treatmentTestSplit}</strong>
-        </div>
-        <div className="App-section">
-          Split <strong>Test_Another_Split</strong> return treatment <strong>{treatmentTestAnotherSplit}</strong>
-        </div>
-        <div className="App-section">
-          Split <strong>Test_Something_Else</strong> return treatment <strong>{treatmentTestSomethingElse}</strong>
-        </div>
-      </div>
-    );
-  }
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { SplitFactory } from '@splitsoftware/splitio-react';
 import './index.css';
+import App from './App';
+import sdkConfig from './sdkConfig';
 
 ReactDOM.render(
-  <App />,
+  <SplitFactory config={sdkConfig}>
+    <App />
+  </SplitFactory>,
   document.getElementById('root')
 );
