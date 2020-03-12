de00faa3c4657d2642bb71f0b8891d8361d8a16a
Emiliano Sanchez <emiliano.sanchez@split.io>
2020-03-12 12:37:21 -0300

    updated example to v1.1.0

--- src/pages/PageUsingHOCs.js
+++ src/pages/PageUsingHOCs.js
@@ -8,6 +8,10 @@ function Loading() {
   return <div>Loading SDK...</div>
 }
 
+function Timedout() {
+  return (<div>SDK timed out (check your API key)</div>);
+};
+
 const TestSplit = withSplitTreatments([feature_1])(
   ({ treatments }) => {
     return (
@@ -21,7 +25,7 @@ const TestSplit = withSplitTreatments([feature_1])(
 
 const OtherSplits = withSplitClient('other_user')(
   withSplitTreatments([feature_2, feature_3])(
-    ({ treatments, isReady }) => {
+    ({ treatments, isReady, isTimedout }) => {
       return isReady ? (
         <div className="App-section">{
           Object.entries(treatments).map(([splitName, treatment]) =>
@@ -31,9 +35,10 @@ const OtherSplits = withSplitClient('other_user')(
             </div>
           )
         }</div>
-      ) : <Loading />
+      ) :
+        isTimedout ? <Timedout /> : <Loading />
     }
-  )
+  ), false, true // updateOnSdkUpdate: false, updateOnSdkTimedout: true, updateOnSdkReady: true (default)
 );
 
 export default function PageUsingHOCs() {
