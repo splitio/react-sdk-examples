bb2bbdac8638d90b207800a92ed3db86ceaaf823
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-05-12 14:53:09 -0300

    replace terms 'split', 'api key' and 'web console'

--- src/pages/PageUsingHOCs.js
+++ src/pages/PageUsingHOCs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withSplitClient, withSplitTreatments } from '@splitsoftware/splitio-react';
-import { feature_1, feature_2, feature_3 } from '../sdkConfig';
+import { feature_flag_1, feature_flag_2, feature_flag_3 } from '../sdkConfig';
 
 /* This example shows withSplitClient and withSplitTreatments HOCs */
 
@@ -9,37 +9,37 @@ function Loading() {
 }
 
 function Timedout() {
-  return (<div>SDK timed out (check your API key)</div>);
+  return (<div>SDK timed out (check your SDK key)</div>);
 };
 
 /* `withSplitTreatments` is a HOC for wrapping render functions inside an SplitTreatments.
- * As `SplitTreatments`, it passes down to a child function a `treatments` prop, which contains split evaluations.
+ * As `SplitTreatments`, it passes down to a child function a `treatments` prop, which contains feature flag evaluations.
  * It also passes down the SDK status (`isReady`, `isTimedout`, `lastUpdate`) as props. You can use `isReady`
  * to conditionally render your component, for example, by showing a Loading label until the SDK is ready.
  * While the SDK is not ready, treatments values are `control`. */
-const TestSplit = withSplitTreatments([feature_1])(
+const TestSplit = withSplitTreatments([feature_flag_1])(
   ({ treatments, isReady }) => {
     return isReady ? (
       <div className='App-section'>
-        <h4>{`Split: ${feature_1}`}</h4>
-        <p>{`Treatment value: ${treatments[feature_1].treatment}`}</p>
+        <h4>{`Feature flag: ${feature_flag_1}`}</h4>
+        <p>{`Treatment value: ${treatments[feature_flag_1].treatment}`}</p>
       </div>
     ) : <Loading />
   }
 );
 
 /* `withSplitClient` is a HOC for `SplitClient`. It changes the SDK client at the context, given a key and a
- * optional traffic type. Thus, the inner `SplitTreatments` components will evaluate splits for a different key */
+ * optional traffic type. Thus, the inner `SplitTreatments` components will evaluate feature flags for a different key */
 const OtherSplits = withSplitClient('other_user')(
   /* In the following piece of UI, we use the `isReady` prop as condition for
    * rendering a `Loading` component until the client with key `other_user` is ready */
-  withSplitTreatments([feature_2, feature_3])(
+  withSplitTreatments([feature_flag_2, feature_flag_3])(
     ({ treatments, isReady, isTimedout }) => {
       return isReady ? (
         <div className='App-section'>{
-          Object.entries(treatments).map(([splitName, treatment]) =>
-            <div key={splitName} >
-              <h4>{`Split: ${splitName}`}</h4>
+          Object.entries(treatments).map(([featureFlagName, treatment]) =>
+            <div key={featureFlagName} >
+              <h4>{`Feature flag: ${featureFlagName}`}</h4>
               <p>{`Treatment value: ${treatment.treatment}`}</p>
             </div>
           )
--- src/sdkConfig.js
+++ src/sdkConfig.js
@@ -1,15 +1,15 @@
 // Your SDK settings for browser.
-// Replace authorizationKey with your client-side API key.
+// Replace authorizationKey with your client-side SDK key.
 const config = {
   core: {
-    authorizationKey: '<your-api-key>',
+    authorizationKey: '<your-sdk-key>',
     key: '<customer-key>'
   }
 };
 
 export default config;
 
-// Replace the following with the name of your splits
-export const feature_1 = 'test_split';
-export const feature_2 = 'test_another_split';
-export const feature_3 = 'test_something_else';
+// Replace the following with the name of your feature flags
+export const feature_flag_1 = 'test_feature_flag';
+export const feature_flag_2 = 'test_another_flag';
+export const feature_flag_3 = 'test_something_else';
