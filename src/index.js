b43fde53a571d0aadb14948808ad0fb41ad30e19
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-11-02 00:37:56 -0300

    Update example to use React SDK v2.0.0

--- src/index.js
+++ src/index.js
@@ -9,12 +9,8 @@ ReactDOM.render(
   /* `SplitFactoryProvider` component inits the SDK with the given config object.
    * The child component and all it's descendants will have access to the SDK functionality.
    * When passing a function as a child, it receives the SDK factory and its status as props. */
-  <SplitFactoryProvider config={sdkConfig} updateOnSdkTimedout={true} >
-    {({ factory, isReady, isTimedout, lastUpdate }) => {
-      // Uncomment the following line if you want to render a different component until the SDK is ready
-      // if (!isReady) return (<div>Loading SDK ...</div>);
-      return <App />
-    }}
+  <SplitFactoryProvider config={sdkConfig} >
+    <App />
   </SplitFactoryProvider>,
   document.getElementById('root')
 );
