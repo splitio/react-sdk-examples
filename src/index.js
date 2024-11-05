17e0866deb8b5409b08ad3cadf6604a9785a2391
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-11-05 10:44:30 -0300

    Update comment regarding functions as a child

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import sdkConfig from './sdkConfig';
 
 ReactDOM.render(
   /* `SplitFactoryProvider` component inits the SDK with the given config object.
-   * The child component and all it's descendants will have access to the SDK functionality.
-   * When passing a function as a child, it receives the SDK factory and its status as props. */
+   * The child component and all it's descendants will have access to the SDK functionality. */
   <SplitFactoryProvider config={sdkConfig} >
     <App />
   </SplitFactoryProvider>,
